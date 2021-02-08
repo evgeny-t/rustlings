f5c73ea17e2541b52252ab23b019f68236678db4
Evgeny Timoshenko <timoshenkoe@yandex-team.ru>
2021-02-09 00:06:42 +0300

    vec, enums, modules, structs

--- exercises/enums/enums3.rs
+++ exercises/enums/enums3.rs
@@ -1,10 +1,12 @@
 // enums3.rs
 // Address all the TODOs to make the tests pass!
 
-// I AM NOT DONE
 
 enum Message {
-    // TODO: implement the message variant types based on their usage below
+    ChangeColor((u8, u8, u8)),
+    Echo(String),
+    Move(Point),
+    Quit,
 }
 
 struct Point {
@@ -27,7 +29,8 @@ impl State {
         self.quit = true;
     }
 
-    fn echo(&self, s: String) {
+    fn echo(&self, mut s: String) {
+        s.push('ę');
         println!("{}", s);
     }
 
@@ -36,7 +39,14 @@ impl State {
     }
 
     fn process(&mut self, message: Message) {
-        // TODO: create a match expression to process the different message variants
+        match message {
+            Message::ChangeColor(color) => self.change_color(color),
+            Message::Echo(msg) => {
+                self.echo(msg);
+            }
+            Message::Move(p) => self.move_position(p),
+            _ => self.quit(),
+        }
     }
 }
 
@@ -51,8 +61,10 @@ mod tests {
             position: Point { x: 0, y: 0 },
             color: (0, 0, 0),
         };
+        let s = String::from("hello world");
         state.process(Message::ChangeColor((255, 0, 255)));
-        state.process(Message::Echo(String::from("hello world")));
+        let m: Message = Message::Echo(s);
+        state.process(m);
         state.process(Message::Move(Point { x: 10, y: 15 }));
         state.process(Message::Quit);
 
